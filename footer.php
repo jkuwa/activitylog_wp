4fbf13019fc16d4e32030ad2439433cf1e3b3ca7
kuwa <163106848+jkuwa@users.noreply.github.com>
2025-04-14 21:47:04 +0900

    追加：テンプレート化

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer class="l-footer p-footer">
+    <small>&copy; junna kuwa All rights reserved.</small>
+  </footer>
+  
+  <script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.min.js" integrity="sha512-CQBWl4fJHWbryGE+Pc7UAxWMUMNMWzWxF4SQo9CgkJIN1kx6djDQZjh3Y8SZ1d+6I+1zze6Z7kHXO7q3UyZAWw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+  <script src="js/main.js"></script>
+</body>
+</html>
