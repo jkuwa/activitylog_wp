487ed8c611b95cff1ec9ad0a5c25417231c32717
kuwa <163106848+jkuwa@users.noreply.github.com>
2025-04-17 20:17:59 +0900

    改善：不要なコードの削除

--- footer.php
+++ footer.php
@@ -1,8 +1,7 @@
-<footer class="l-footer p-footer">
+  <footer class="l-footer p-footer">
     <small>&copy; junna kuwa All rights reserved.</small>
   </footer>
   
-  <!-- <script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js"></script> -->
   <?php wp_footer(); ?>
 </body>
 </html>
