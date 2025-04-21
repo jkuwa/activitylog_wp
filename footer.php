d1fa07d103b90d86ed2ad42a787242165d579ff2
kuwa <163106848+jkuwa@users.noreply.github.com>
2025-04-21 17:32:27 +0900

    追加：License情報

--- footer.php
+++ footer.php
@@ -2,6 +2,12 @@
     <small>&copy; junna kuwa All rights reserved.</small>
   </footer>
   
+  /*!
+  * Chart.js v4.4.1
+  * https://www.chartjs.org
+  * (c) 2023 Chart.js Contributors
+  * Released under the MIT License
+  */
   <?php wp_footer(); ?>
 </body>
 </html>
