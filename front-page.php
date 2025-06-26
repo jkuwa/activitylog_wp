a5f73b2b18f047ca882a28cc67535503669bc0cd
kuwa <163106848+jkuwa@users.noreply.github.com>
2025-06-26 23:40:44 +0900

    修正：コンテンツ背景

--- front-page.php
+++ front-page.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-  <main class="l-main">
+  <main class="l-main p-main">
     <!-- ABOUT SECTION -->
 
     <?php
@@ -33,7 +33,7 @@
     <?php endif; ?>
     </section>
 
-    <div class="p-contents js-target">
+    <div class="p-contents">
       <div class="p-contents__tabs" role="tablist" aria-label="活動記録タブ">
         <div class="c-inner">
           <button id="tab_log" class="c-button--tab js-tabBtn is-current" type="button" role="tab" aria-selected="true" aria-controls="log" tabindex="0" data-tab="log">log</button>
@@ -41,57 +41,59 @@
         </div>
       </div>
 
-      <!-- LOG SECTION -->
-      <section id="log" class="c-content p-log js-tabContent is-open" role="tabpanel" aria-labelledby="tab_log" tabindex="0" data-tab="log">
-        <div class="c-inner">
-          <!-- CALENDAR -->
-          <div class="p-calendar js-calendar js-target"></div>
+      <div class="p-contents__content">
+        <!-- LOG SECTION -->
+        <section id="log" class="c-content p-log js-tabContent is-open" role="tabpanel" aria-labelledby="tab_log" tabindex="0" data-tab="log">
+          <div class="c-inner">
+            <!-- CALENDAR -->
+            <div class="p-calendar js-calendar js-target"></div>
 
-          <!-- ARCHIVE -->
-          <ul class="p-log__archive js-archive"></ul>
-        </div>
-
-        <a href="#log" class="c-button--top js-topBtn">log top</a>
-      </section>
-
-      <!-- REPORT SECTION -->
-      <div id="report" class="c-content p-report js-tabContent" role="tabpanel" aria-labelledby="tab_report" tabindex="0" data-tab="report">
-        <div class="c-inner">
-          <div class="p-report__head">
-            <fieldset>
-              <label><input type="radio" name="mode" value="month" checked>月間</label>
-              <label><input type="radio" name="mode" value="year">年間</label>
-            </fieldset>
-  
-            <div class="p-report__select">
-              <input type="month" id="month" class="js-select is-open">
-              <select name="year" class="js-select">
-                <option value="2025" selected>2025年</option>
-                <option value="2024">2024年</option>
-              </select>
-              <button class="c-button--select js-setBtn">select</button>
-            </div>
+            <!-- ARCHIVE -->
+            <ul class="p-log__archive js-archive"></ul>
           </div>
-  
-          <section class="p-graph">
-            <h2>activity time</h2>
 
-            <div class="p-graph__contents">
-              <div class="p-graph__time">
-                <canvas class="js-chart" aria-label="学習時間グラフ"></canvas>
-              </div>
+          <a href="#log" class="c-button--top js-topBtn">log top</a>
+        </section>
 
-              <div class="p-graph__ranking">
-                <table class="p-table">
-                  <tbody class="js-table"></tbody>
-                </table>
-                <dl>
-                  <dt>合計</dt>
-                  <dd class="c-hours js-total"></dd>
-                </dl>
+        <!-- REPORT SECTION -->
+        <div id="report" class="c-content p-report js-tabContent" role="tabpanel" aria-labelledby="tab_report" tabindex="0" data-tab="report">
+          <div class="c-inner">
+            <div class="p-report__head">
+              <fieldset>
+                <label><input type="radio" name="mode" value="month" checked>月間</label>
+                <label><input type="radio" name="mode" value="year">年間</label>
+              </fieldset>
+    
+              <div class="p-report__select">
+                <input type="month" id="month" class="js-select is-open">
+                <select name="year" class="js-select">
+                  <option value="2025" selected>2025年</option>
+                  <option value="2024">2024年</option>
+                </select>
+                <button class="c-button--select js-setBtn">select</button>
               </div>
             </div>
-          </section>
+    
+            <section class="p-graph">
+              <h2>activity time</h2>
+
+              <div class="p-graph__contents">
+                <div class="p-graph__time">
+                  <canvas class="js-chart" aria-label="学習時間グラフ"></canvas>
+                </div>
+
+                <div class="p-graph__ranking">
+                  <table class="p-table">
+                    <tbody class="js-table"></tbody>
+                  </table>
+                  <dl>
+                    <dt>合計</dt>
+                    <dd class="c-hours js-total"></dd>
+                  </dl>
+                </div>
+              </div>
+            </section>
+          </div>
         </div>
       </div>
 
